8a3a3c089f0c7d69a6b5c93523bb38610c6fa361
Karthik Raj <karthikraj9000@gmail.com>
2026-05-28 14:22:33 +0530

    Add asyncHandler middleware to catch unhandled async errors

---
+++ buildboard-backend/middleware/asyncHandler.js
@@ -0,0 +1,9 @@
+/**
+ * Async Handler Wrapper
+ * Wraps async route handlers to catch unhandled promise rejections
+ */
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+module.exports = asyncHandler;
